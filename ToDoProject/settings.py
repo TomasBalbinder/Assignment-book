e077ec25497e0a1e7cd8031d17558df916a0f54f
Tomas <tomasbalbinder@gmail.com>
2023-02-22 01:17:48 +0100

    deploy

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -15,13 +15,8 @@ import os
 import environ
 
 
-env = environ.Env()
-environ.Env.read_env()
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
