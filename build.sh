8df1f97ab4206203dcd67c0034b7bd7e64da4948
Tomas <tomasbalbinder@gmail.com>
2023-02-25 10:45:50 +0100

    deploy

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
+if [ "$CREATE_SUPERUSER" = true ]; then
+  python manage.py createsuperuser --noinput
 fi
