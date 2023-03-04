0227d3581fd98d50f98fceb569922d219e047928
Tomas <tomasbalbinder@gmail.com>
2023-03-04 19:38:44 +0100

    add

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
-set -o errexit  # exit on error
+#set -o errexit  # exit on error
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+#python manage.py collectstatic --no-input
+#python manage.py migrate
 python manage.py openai
 
-if [ "$CREATE_SUPERUSER" ]; then
+"""if [ "$CREATE_SUPERUSER" ]; then
   python manage.py createsuperuser --noinput
-fi
+fi"""
 
 
