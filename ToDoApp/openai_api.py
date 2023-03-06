4b66acef691af3f1f17aa96f88ffebcc8742fc45
Tomas <tomasbalbinder@gmail.com>
2023-03-06 18:53:46 +0100

    api

--- ToDoApp/openai_api.py
+++ ToDoApp/openai_api.py
@@ -1,34 +1,36 @@
 import openai
 import urllib.request
 import datetime,time
-import environ
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 def openai_image():
-  response = openai.Image.create(
+    
+    response = openai.Image.create(
     prompt="animal in the library , digital art, fairytale",
     n=1,
     size="512x512"
-  )
-  image_url = response['data'][0]['url']
+    )
+    image_url = response['data'][0]['url']
 
-  url = image_url
+    url = image_url
 
-  download_image = urllib.request.urlretrieve(url, "ToDoProjectFolder\ToDoApp\static\ToDoApp\openai_image.png")
+    download_image = urllib.request.urlretrieve(url, "ToDoProjectFolder\ToDoApp\static\ToDoApp\openai_image.png")
 
-  return download_image
+    return download_image
 
+openai_image()
 
-
-while True: 
-    target_time = datetime.datetime.now().replace(hour=22, minute=38, second=00, microsecond=0)
+"""while True: 
+    target_time = datetime.datetime.now().replace(hour=1, minute=2, second=55, microsecond=0)
 
 
     while datetime.datetime.now() < target_time:
         openai_image()
-        time.sleep(360)
-        
+        time.sleep(10)
+        """
 
 
