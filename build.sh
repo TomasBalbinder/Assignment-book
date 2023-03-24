6b90515fd506a33aca4d691e696a63436c92c0dc
Tomas <tomasbalbinder@gmail.com>
2023-03-24 12:14:46 +0100

    api

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+python manage.py makemigrations
+python manage.py migrate 
 
 
 if [ "$CREATE_SUPERUSER" ]; then
