f899222cd27562d18ca33aa18113c6aeff378294
Tomas <tomasbalbinder@gmail.com>
2023-03-26 11:42:40 +0200

    api

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.pz makemigrations --no-input
 python manage.py migrate --no-input
 
 
